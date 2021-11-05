48d00e259e94fb9f128ab6822a6b8f344e7ab5bb
Hiranika <h1r4n1k4@gmail.com>
2021-11-06 01:19:10 +0700

    Tidy up code

--- luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_info.lua
+++ luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_info.lua
@@ -4,12 +4,11 @@ local DISP = require "luci.dispatcher"
 local b
 
 --SimpleForm for Info
-b = SimpleForm("amlogic", translate("Amlogic Service"), nil)
+b = SimpleForm("amlogic", nil)
+b.title = translate("Amlogic Service")
 b.description = translate("Supports management of Amlogic s9xxx boxes.")
 b.reset = false
 b.submit = false
 b:section(SimpleSection).template  = "amlogic/other_info"
 
-
 return b
-
