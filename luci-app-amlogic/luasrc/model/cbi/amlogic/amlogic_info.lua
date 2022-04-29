284328222bff8465685728f19733dd34dc69c2be
Hiranika <h1r4n1k4@gmail.com>
2022-04-29 19:22:01 +0700

    Unicode Indent Format

--- luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_info.lua
+++ luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_info.lua
@@ -1,14 +1,12 @@
-local fs = require "luci.fs2"
-local http = require "luci.http"
-local DISP = require "luci.dispatcher"
 local b
 
 --SimpleForm for Info
-b = SimpleForm("amlogic", nil)
-b.title = translate("Amlogic Service")
-b.description = translate("Supports Management of Amlogic Chipset.")
-b.reset = false
-b.submit = false
-b:section(SimpleSection).template  = "amlogic/other_info"
+b             = SimpleForm("amlogic", nil)
+b.title       = translate("Amlogic Service")
+b.description = translate("Supports Management of Amlogic OpenWrt.")
+b.reset       = false
+b.submit      = false
+
+b:section(SimpleSection).template = "amlogic/other_info"
 
 return b
