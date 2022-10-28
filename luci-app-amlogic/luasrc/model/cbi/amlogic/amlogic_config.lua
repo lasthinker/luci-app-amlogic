ab8eedc29aefb36b61aa1e2c96074c2105ca487a
Hiranika <h1r4n1k4@gmail.com>
2022-10-28 11:06:53 +0700

    Adjust tag name

--- luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_config.lua
+++ luci-app-amlogic/luasrc/model/cbi/amlogic/amlogic_config.lua
@@ -37,7 +37,7 @@ firmware_repo.rmempty = false
 --3.Set OpenWrt Releases's Tag Keywords
 firmware_tag = o:option(Value, "amlogic_firmware_tag", translate("Keywords of Tags in Releases:"))
 firmware_tag.description = translate("Set the keywords of Tags in Releases of github.com in [Online Download Update].")
-firmware_tag.default = "s9xxx_lasthinker"
+firmware_tag.default = "aml_lasthinker"
 firmware_tag.rmempty = false
 
 --4.Set OpenWrt Firmware Suffix
@@ -62,8 +62,6 @@ kernel_branch.description = translate("Set the version branch of the openwrt fir
 kernel_branch:value("5.4", translate("5.4"))
 kernel_branch:value("5.10", translate("5.10"))
 kernel_branch:value("5.15", translate("5.15"))
-kernel_branch:value("5.18", translate("5.18"))
-kernel_branch:value("5.19", translate("5.19"))
 local default_kernel_branch = luci.sys.exec("ls /lib/modules/ 2>/dev/null | grep -oE '^[1-9].[0-9]{1,3}'")
 kernel_branch.default = trim(default_kernel_branch)
 kernel_branch.rmempty = false
